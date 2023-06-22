a5371c3218e10bf47ac1bbe60261b22bf99175ca
dudghfb1 <dudghfb1@gmail.com>
2023-06-22 11:34:37 +0900

    22일 예제 추가

---
+++ Ch05/src/sub7/Animal.java
@@ -0,0 +1,14 @@
+package sub7;
+
+public class Animal {
+	
+	// 오버라이드를 위한 메서드 선언
+	public void hunt() {
+		
+		
+	}
+	public void move() {
+		System.out.println("Animal move...");
+		
+	}
+}
---
+++ Ch05/src/sub9/Car.java
@@ -0,0 +1,9 @@
+package sub9;
+
+public interface Car {
+
+	public abstract void speedUp(int speed);
+	public abstract void speedDown(int speed);
+	public void show();// abstract는 생략 가능
+	
+}
