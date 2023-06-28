b9551a83462314469330d8e0d41ba71a44f7e1dc
dudghfb1 <dudghfb1@gmail.com>
2023-06-28 13:59:51 +0900

    6/28일 수업예제 추가

---
+++ Ch08/src/sub2/QueueTest.java
@@ -0,0 +1,30 @@
+package sub2;
+
+import java.util.Queue;
+import java.util.LinkedList;
+
+/**
+ * 날짜 : 2023/06/28
+ * 이름 : 류영호
+ * 내용 : Java 자료구조 스택 실습하기
+ * 
+ * 큐(Queue)
+ *  - 큐는 데이터가 차례대로 쌓이는 자료구조(FIFO:선입선출)
+ *  - 이벤트, 애니메이션 실행에 사용
+ */
+public class QueueTest {
+	public static void main(String[] args) {
+		
+		Queue<Integer> queue = new LinkedList<>();
+		
+		queue.offer(1);
+		queue.offer(2);
+		queue.offer(3);
+		queue.offer(4);
+		queue.offer(5);
+		
+		while(!queue.isEmpty()) {
+			System.out.println(queue.poll());
+		}
+	}
+}
