63b4c81b4d633acbca69e1e2c65681668a38e127
dudghfb1 <dudghfb1@gmail.com>
2023-06-29 11:47:30 +0900

    수업예제 추가

---
+++ Ch09/src/sub3/DeserializeTest.java
@@ -0,0 +1,44 @@
+package sub3;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+
+/**
+ * 날짜 : 2023/06/29
+ * 이름 : 류영호
+ * 내용 : Java 버퍼 입출력 스트림 실습하기
+ */
+
+public class DeserializeTest {
+
+	public static void main(String[] args) {
+		
+		String path = "C:\\Users\\Java\\Desktop\\Apple.txt";
+		
+		try {
+			// 스트림 생성
+			FileInputStream fis = new FileInputStream(path);
+			ObjectInputStream ois = new ObjectInputStream(fis);
+			
+			// 역직렬화 /다운캐스팅 apple(Apple)
+			Apple apple = (Apple)ois.readObject();
+			apple.show();
+			
+			// 스트림 해제
+			ois.close();
+			fis.close();
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("프로그램 종료");
+	}
+}
